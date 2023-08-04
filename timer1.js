bfa22e0defb215946bce67a4d21a2cae9bee32f6
Casandra Normand <casandra.normand@outlook.com>
2023-08-03 23:16:36 -0400

    ignore negative numbers & strings

--- timer1.js
+++ timer1.js
@@ -1,3 +1,5 @@
 for (let i = 2; i < process.argv.length; i++){
-  setTimeout((() => {process.stdout.write('\x07');}), (process.argv[i] * 1000));
+  if (!isNaN(process.argv[i]) && process.argv[i] >= 0 ) {
+    setTimeout((() => {process.stdout.write('\x07');}), (process.argv[i] * 1000));
+  }
 }
